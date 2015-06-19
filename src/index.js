4423508285927d2fe69b0a4f36c8053a37cb897d
Bradford Powell <bpow@drpowell.org>
2015-06-19 08:25:10 -0400

    actually handle nested expressions properly

--- src/index.js
+++ src/index.js
@@ -28,9 +28,9 @@ exports.pass = function(ast, options) {
     var generate_code = generate_code_without_undefined;
 
     function process_expression(exp, stack) {
+        var stack_plus = stack.concat(exp);
         if ("action" !== stack[stack.length-1].type) {
             if ("sequence" === exp.type) {
-                var stack_plus = stack.concat(exp);
                 var labels = [];
                 exp.elements = exp.elements.map(function (e) {
                     if ("labeled" === e.type) labels.push(e.label);
@@ -49,17 +49,20 @@ exports.pass = function(ast, options) {
                         code: generate_code(labels),
                         expression: exp
                     };
+                    stack_plus = stack.concat(exp);
                 }
             } // sequence
-            if ("choice" === exp.type) {
-                var next_stack = stack.concat(exp);
-                exp.alternatives = exp.alternatives.map(function (alt) {
-                    return process_expression(alt, next_stack);
-                });
-            } // choice
         }
         if (exp.expression) {
             exp.expression = process_expression(exp.expression, stack.concat(exp));
+        } else if (exp.alternatives) { // choice
+            exp.alternatives = exp.alternatives.map(function (alt) {
+                return process_expression(alt, stack_plus);
+            });
+        } else if (exp.elements) { // sequence 
+            exp.elements = exp.elements.map(function (e) {
+                return process_expression(e, stack_plus);
+            });
         }
         return exp;
     }
